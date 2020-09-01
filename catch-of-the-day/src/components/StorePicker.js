c9819d2522af00b62d4f0df2dc375dd1572410df
srzvan <sbingu.razvan@gmail.com>
2020-09-01 16:12:10 +0300

    wrapped emoji in a <span role=img />

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -7,7 +7,12 @@ class StorePicker extends React.Component {
         <h2>Please enter a store name</h2>
         <label htmlFor="store-name">Store name</label>
         <input id="store-name" type="text" required />
-        <button type="submit">Visit store 👉</button>
+        <button type="submit">
+          Visit store
+          <span role="img" aria-label="Pointing finger emoji">
+            👉
+          </span>
+        </button>
       </form>
     );
   }
