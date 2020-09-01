c3c5f5c4a6ba0a2791e87c2df0088e684a85cbeb
srzvan <sbingu.razvan@gmail.com>
2020-09-01 16:38:33 +0300

    added default value for <StorePicker /> input, creted ref for input, created scaffold submit fn

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,20 @@
 import React from "react";
 
+import { getFunName } from "../helpers";
+
 class StorePicker extends React.Component {
+  storeNameInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store name</h2>
         <label htmlFor="store-name">Store name</label>
-        <input id="store-name" type="text" required />
+        <input ref={this.storeNameInput} id="store-name" type="text" defaultValue={getFunName()} required />
         <button type="submit">
           Visit store
           <span role="img" aria-label="Pointing finger emoji">
