9e0c820e6d34c9de843a3f36b5d5b99ac2630355
srzvan <sbingu.razvan@gmail.com>
2020-09-01 16:11:29 +0300

    <Router /> component & index.js now renders the router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
 
-import App from "./components/App";
+import Router from "./components/Router";
 
 import "./css/style.css";
 
 const mountPoint = document.getElementById("main");
-render(<App />, mountPoint);
+render(<Router />, mountPoint);
