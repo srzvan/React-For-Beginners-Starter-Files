71fa26963a9a3ad514481edd66f9e70b43cc3e4b
srzvan <sbingu.razvan@gmail.com>
2020-09-02 18:55:54 +0300

    added state to <App /> & addFish method

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,14 +5,32 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+
+    // 2. Add the new fish to fishes
+    //    using a timestamped key
+    //    to distinguish between fishes
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3. Update state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
