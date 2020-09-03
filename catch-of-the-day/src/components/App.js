271ef30b720440398c72d26ca8354fccb78eb4f2
srzvan <sbingu.razvan@gmail.com>
2020-09-03 18:55:11 +0300

    button is now disabled based on fish status prop, addToOrder method in <App /> & <Fish />

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -26,6 +26,16 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order or update
+    //    the number of the items
+    order[key] = order[key] + 1 || 1;
+    // 3. Update state
+    this.setState({ order });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -37,7 +47,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
             ))}
           </ul>
         </div>
