9b465c2bb6f005fdb8dd9e43b4098437acca4a03
srzvan <sbingu.razvan@gmail.com>
2020-09-04 15:44:09 +0300

    the order is now persisted through localStorage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -17,11 +17,22 @@ class App extends React.Component {
   componentDidMount() {
     const { params } = this.props.match;
 
+    // reinstate localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+
+    if (localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
+
     // sync changes between <App />'s state & DB
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes",
     });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(`${this.props.match.params.storeId}`, JSON.stringify(this.state.order));
   }
 
   componentWillUnmount() {
