a2a96f95bc3b42d60bcfac1c691c4f21aa5ec5bf
srzvan <sbingu.razvan@gmail.com>
2020-09-04 15:44:39 +0300

    fix for trying to render order before the fishes are synced with firestore db

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -6,7 +6,20 @@ class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const quantity = this.props.order[key];
-    const isAvailable = fish.status === "available";
+    const isAvailable = fish && fish.status === "available";
+
+    // the order is persisted using localStorage
+    // the fishes are persisted using firebase
+    //
+    // because localStorage is local it will
+    // be used to render the order immediately
+    // even though the fishes did not sync yet
+    // (the fishes are initialiazed through
+    // a network request against the firebase DB)
+
+    // make sure the fish is loaded before rendering
+    if (!fish) return null;
+    // returning null doesn't render anything
 
     if (!isAvailable) {
       return <li key={key}>Sorry, {fish ? fish.name : "fish"} is no longer available</li>;
